bfb707d34f8cfc9af8425ca15dc837fcb7d4e5ea
Mr. Hands <mrhands31@gmail.com>
2020-03-28 11:02:39 +0100

    Find save file directory when clicking Browse.

--- MonsterCrusher/MainViewModel.cs
+++ MonsterCrusher/MainViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,7 @@ namespace MonsterCrusher
     {
         public String SettingsPath
         {
-            get { return AppSettings.Instance.settingsPath; }
+            get { return ConfigurationManager.AppSettings["settingsPath"]; }
         }
     }
 }
--- MonsterCrusher/MainWindow.xaml.cs
+++ MonsterCrusher/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace MonsterCrusher
 {
@@ -25,6 +28,27 @@ namespace MonsterCrusher
             InitializeComponent();
 
             DataContext = new MainViewModel();
+        }
+
+        private void btnBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Monster Girl Club Bifrost Save Files (*.dat)|*.dat";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                ConfigurationManager.AppSettings["settingsPath"] = System.IO.Path.GetDirectoryName(openFileDialog.FileName);
+                DataContext = new MainViewModel();
+
+                /*var fileStream = openFileDialog.OpenFile();
+                StreamReader reader = new StreamReader(fileStream);
+                fileContent = reader.ReadToEnd();*/
+            }
         }
     }
 }
